556a1bad444ad75873258f1adbc4c4f620833dac
Steve Anderson <steve@bootladder.com>
2019-06-07 21:44:31 -0400

    clean

--- extract_tabs.py
+++ extract_tabs.py
@@ -6,51 +6,6 @@ import time
 from myutil import *
 
 
-def do_cool_stuff(img):
-    img_before_contours = img
-    contours, hierarchy = cv2.findContours(img_before_contours,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-    img_contours = cv2.drawContours(np.ones_like(img)*255, contours, -1, (0,255,0), 1)
-
-    img_blank = np.ones_like(img_contours)*255
-    catter = np.ones((20,20),np.float32)/(100)
-
-    tabGlyphs = []
-
-    for c in contours:
-
-        # compute the center of the contour
-        M = cv2.moments(c)
-        if M["m00"] == 0:
-            continue
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-
-        area = cv2.contourArea(c)
-        print 'area of contour: ' , area
-        if area > 200 or area < 100:
-            print 'rejecting area ' , area
-            continue
-
-        # draw the center of the shape on the image
-        cv2.circle(img_blank, (cX, cY), 5, (0, 0, 255))
-
-        # cut the sub-image out of the original
-        imgSection = img[cY-10:cY+10,cX-10:cX+10]
-
-        # add to the list
-        tabGlyphs.append(imgSection)
-
-        # draw it
-        img_blank[cY-10:cY+10,cX-10:cX+10] = imgSection
-        #cv2.imshow('image',img_blank)
-        cv2.waitKey(100)
-        catter = np.concatenate((catter, imgSection), axis=1)
-        #cv2.imshow('image',imgSection)
-
-    return img_blank
-
-
 def filter_contours_by_area(contours):
 
     #get average, mean, mode area
@@ -132,8 +87,8 @@ img = img_orig = cv2.imread(sys.argv[1],0)
 
 #pre process
 img = ~img #do everything with white on black
-img = remove_lines_horizontal(img)
-img = remove_lines_vertical(img)
+img = img_rm_lines_h = remove_lines_horizontal(img)
+img = img_rm_lines_v = remove_lines_vertical(img)
 img = img_dilate = cv2.dilate(img,kernel_square(3),iterations = 1)
 img = img_laplace = laplace(img)
 img = img_dilate = cv2.dilate(img,kernel_square(3),iterations = 1)
@@ -145,12 +100,12 @@ img = img_open2 = morph_close_square(img,4)
 #img = img_with_contours = do_cool_stuff(img)
 
 
-#contours
+#contours 2 glyphs
 contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 contours = accepted_contours = filter_contours_by_area(contours)
-print 'there are %d contours' % len(accepted_contours)
 contours = sorted_contours = sort_contours_by_column_position(contours)
 glyphs = map_contours_to_glyphs(contours, img_orig)
-showlisthorizontal(glyphs)
 
+print 'there are %d contours' % len(accepted_contours)
+showlisthorizontal(glyphs)
 showlist((img_orig, img_laplace,img_thresh, img_smooth, img_thresh2,img_open2))
