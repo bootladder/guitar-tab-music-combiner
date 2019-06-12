835ee9642752baed2e9d4d1a07b7b540c1b53ba8
Steve Anderson <steve@bootladder.com>
2019-06-11 23:45:07 -0400

    convert draw_tabglyphs_on_music to function

--- draw_tabglyphs_on_music.py
+++ draw_tabglyphs_on_music.py
@@ -8,9 +8,6 @@ from extract_tabs import *
 from extract_music import *
 
 
-def draw_tabglyphs_on_music(img_tab, img_music):
-
-    return
 
 
 def draw_glyphs_on_image(glyphs, coords, img):
@@ -21,33 +18,43 @@ def draw_glyphs_on_image(glyphs, coords, img):
 
 ############################################################
 
-# Load Images
-img_music =  cv2.imread(sys.argv[1],0)
-img_tab   =  cv2.imread(sys.argv[2],0)
-
-glyphs = imgtab_to_glyphs(img_tab)
-coords = imgmusic_to_notecoordinates(img_music)
-
-print 'Found ', len(glyphs), ' tab notes and ',len(coords), 'music notes'
-if len(glyphs) != len(coords):
-    print 'NOT THE SAME!!'
-    sys.exit(1)
+def draw_tabglyphs_on_music(img_music, img_tab):
 
 
-for i in range(0,len(coords)):
-    coord = coords[i]
-    col = coord[0]
-    row = coord[1]
+    glyphs = imgtab_to_glyphs(img_tab)
+    coords = imgmusic_to_notecoordinates(img_music)
 
-    glyphRowSize = len(glyphs[i])
-    glyphColSize = len(glyphs[i][0])
-    #print 'Glyph Size: rows ',glyphRowSize,' col ',glyphColSize
+    print 'Found ', len(glyphs), ' tab notes and ',len(coords), 'music notes'
+    if len(glyphs) != len(coords):
+        print 'NOT THE SAME!!'
+        sys.exit(1)
 
-    coord_row_start = (row - (glyphRowSize/2))
-    coord_col_start = (col - (glyphColSize/2))
 
-    img_music[ coord_row_start:coord_row_start+glyphRowSize,
-               coord_col_start:coord_col_start+glyphColSize] = glyphs[i]
-    #print 'row ',row,' col ',col
+    for i in range(0,len(coords)):
+        coord = coords[i]
+        col = coord[0]
+        row = coord[1]
 
-show(img_music)
+        glyphRowSize = len(glyphs[i])
+        glyphColSize = len(glyphs[i][0])
+        #print 'Glyph Size: rows ',glyphRowSize,' col ',glyphColSize
+
+        coord_row_start = (row - (glyphRowSize/2))
+        coord_col_start = (col - (glyphColSize/2))
+
+        img_music[ coord_row_start:coord_row_start+glyphRowSize,
+                coord_col_start:coord_col_start+glyphColSize] = glyphs[i]
+        #print 'row ',row,' col ',col
+
+    return img_music
+
+
+if __name__ == '__main__':
+
+    # Load Images
+    img_music =  cv2.imread(sys.argv[1],0)
+    img_tab   =  cv2.imread(sys.argv[2],0)
+
+    img = draw_tabglyphs_on_music(img_music,img_tab)
+
+    show(img)
